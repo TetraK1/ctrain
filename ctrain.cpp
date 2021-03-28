feabea0f8f4cbc4fcebb9895dc85560c6f6b98d0
TetraK1 <13362609+TetraK1@users.noreply.github.com>
2021-03-28 23:41:53 +0100

    Update

--- ctrain.cpp
+++ ctrain.cpp
@@ -6,38 +6,58 @@
 using json = nlohmann::json;
 using namespace std;
 
-class SimpleNode {
+class RailObject {
     public:
         const string id;
+        RailObject(const string &id):
+            id(id)
+        {}
+
+        virtual ~RailObject() {}
+
+        friend ostream& operator<<(ostream& out, const RailObject& ro) {
+            return out << "<RailObject: " << ro.id << ">";
+        }
+
+        static shared_ptr<RailObject> make(const json& j);
+};
+
+class SimpleNode: public RailObject {
+    public:
         const string t1;
         const string t2;
+
+        SimpleNode(const string& id, const string& t1, const string& t2):
+            RailObject(id),
+            t1(t1),
+            t2(t2)
+        {}
 };
 
-class EndNode {
+class EndNode: public RailObject {
     public:
-        const string id;
-        const string t;
+        const string track;
 
-    EndNode(const string &id, const string &t):
-        id{id},
-        t{t}
-    {}
+        EndNode(const string &id, const string &track): 
+            RailObject(id),
+            track(track)
+        {}
 
-    EndNode(const json &j) : EndNode(j.at("id"), j.at("track")){}
+        EndNode(const json &j) : EndNode(j.at("id"), j.at("track")){}
 
-    string toString() const {return "<EndNode: " + id + ">";}
-    friend ostream& operator<<(ostream &out, const EndNode &en){return out << en.toString();}
+        friend ostream& operator<<(ostream &out, const EndNode &en) {
+            return out << "<EndNode: " << en.id << ">";
+        }
 };
 
-class PointsNode {
+class PointsNode: public RailObject {
     public:
-        const string id;
         const string facing_track;
         const string forward_track;
         const string reverse_track;
 
-        PointsNode(const string &id, const string &facing_track, const string &forward_track, const string &reverse_track):
-            id(id),
+        PointsNode(const string &id, const string &facing_track, const string &forward_track, const string &reverse_track): 
+            RailObject(id),
             facing_track(facing_track),
             forward_track(forward_track),
             reverse_track(reverse_track)
@@ -45,83 +65,79 @@ class PointsNode {
 
         PointsNode(const json &j): PointsNode(j.at("id"), j.at("facing"), j.at("forward"), j.at("reverse")){}
 
-        string toString() const{return "<PointsNode: " + id + ">";}
-        friend ostream& operator<<(ostream &out, const PointsNode &pn){return out << pn.toString();}
+        friend ostream& operator<<(ostream &out, const PointsNode &pn){
+            return out << "<PointsNode: " << pn.id << ">";
+        }
 };
 
-class Track {
+class Track: public RailObject {
     public:
-        const string id;
         const string start_node;
         const string end_node;
+        const double length;
 
-        Track(const string &id, const string &start_node, const string &end_node):
-            id(id),
+        Track(const string &id, const string &start_node, const string &end_node, const double& length):
+            RailObject(id),
             start_node(start_node),
-            end_node(end_node)
+            end_node(end_node),
+            length(length)
         {}
 
-        Track(const json &j): Track(j.at("id"), j.at("start_node"), j.at("end_node")){}
+        Track(const json &j): Track(j.at("id"), j.at("start_node"), j.at("end_node"), j.at("length")){}
 
-        string toString() const{return "<Track: " + id + ">";}
-        friend ostream& operator<<(ostream &out, const Track &l){return out << l.toString();}
+        friend ostream& operator<<(ostream &out, const Track &t){
+            return out << "<Track: " << t.id << ", length: " << t.length << ">";
+        }
 };
+
+shared_ptr<RailObject> RailObject::make(const json& j) {
+    const string& type = j["type"];
+    if (type == "end_node") return make_shared<EndNode>(j);
+    if (type == "points_node") return make_shared<PointsNode>(j);
+    if (type == "track") return make_shared<Track>(j);
+    return nullptr;
+}
 
 class Layout {
     public:
-        vector<EndNode> end_nodes;
-        vector<PointsNode> points_nodes;
-        vector<Track> tracks;
-
-        Layout(vector<EndNode> end_nodes, vector<PointsNode> points_nodes, vector<Track> tracks):
-        end_nodes(end_nodes),
-        points_nodes(points_nodes),
-        tracks(tracks)
-        {}
+        map<string, shared_ptr<RailObject>> objects;
 
         Layout(const json &j){
-            for (auto en: j.at("end_nodes")) {end_nodes.push_back(EndNode(en));}
-            for (auto pn: j.at("points_nodes")) {points_nodes.push_back(PointsNode(pn));}
-            for (auto t: j.at("tracks")) {tracks.push_back(Track(t));}
-        }
-
-        Track get_track(const string& t1_id, const string& t2_id){
-            for (auto t: tracks){
-                if (t1_id == t.start_node && t2_id == t.end_node) return t;
-                if (t2_id == t.start_node && t1_id == t.end_node) return t;
+            for (auto jo: j){
+                shared_ptr<RailObject> ro(RailObject::make(jo));
+                objects.insert(pair<string, shared_ptr<RailObject>>(ro->id, ro));
             }
-            throw std::exception("No matching track");
         }
 
         friend ostream& operator<<(ostream &out, const Layout &l){
             out << "Layout:\n";
-            out << "    End nodes:\n";
-            for (auto en: l.end_nodes) {out << "        " << en << '\n';}
-            out << "    Points nodes:\n";
-            for (auto pn: l.points_nodes) {out << "        " << pn << '\n';}
-            out << "    Tracks:\n";
-            for (auto t: l.tracks) {out << "        " << t << '\n';}
+            for(auto ro: l.objects){
+                out << "\t" << *(ro.second) << "\n";
+            }
             return out;
         }
 };
 
 Layout load_layout_from_file(const string &fname){
-    ifstream infile { fname };
-    string file_contents { istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
-    json j = json::parse(file_contents);
+    ifstream infile(fname);
+    json j;
+    infile >> j;
     return Layout(j);
 }
 
 int main(){
-    std::unique_ptr<Layout> l(nullptr);
-
+    unique_ptr<Layout> l;
     try {
-        l = std::make_unique<Layout>(load_layout_from_file("layout.json"));
-    }
-    catch (...) {
-        cout << "Error loading layout\n";
+        l = make_unique<Layout>(load_layout_from_file("layout.json"));
+    } catch (exception& e) {
+        cout << "Loading failed\n";
+        cout << e.what() << '\n';
         throw;
     }
-    cout << *l;
-    cout << l->get_track("PN001", "EN001");
+    cout << *l << "\n";
+    for (auto o: l->objects){
+        shared_ptr<Track> t = dynamic_pointer_cast<Track>(o.second);
+        if (!t) continue;
+        cout << *t << "\n";
+    }
 }
